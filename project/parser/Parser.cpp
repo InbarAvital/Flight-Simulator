d0b94d33017e0009c8576dd12665085cca6c065f
noamyakov <59260056+noamyakov@users.noreply.github.com>
2019-12-31 00:02:19 +0200

    Finished Parser

---
+++ project/parser/Parser.cpp
@@ -0,0 +1,21 @@
+//
+// Created by noam on 30/12/2019.
+//
+
+#include "Parser.h"
+
+void Parser::AddCommand(const string &name, Command *command) {
+  this->commands[name] = command;
+}
+
+void Parser::Parse() {
+  int index = 0;
+
+  while (index < this->tokens.size()) {
+    // Analyzes the current command.
+    Command *command = this->commands[this->tokens[index]];
+
+    // Executes the command and move to the next one.
+    index += command->Execute(this->tokens, index);
+  }
+}
---
+++ project/parser/Parser.h
@@ -0,0 +1,36 @@
+//
+// Created by noam on 30/12/2019.
+//
+
+#ifndef EX3_PARSER_PARSER_H_
+#define EX3_PARSER_PARSER_H_
+
+#include <vector>
+#include <string>
+#include <map>
+#include "../commands/Command.h"
+
+using namespace std;
+
+// The Parser is in charge of analyzing the lexical tokens
+// and executing the commands in the given order.
+class Parser {
+ public:
+  // Input: t - The lexical tokens.
+  // Constructs a Parser with the given lexical tokens.
+  explicit Parser(vector<string> &t): tokens(t) {}
+
+  // Input: name - The lexeme activates a specific command.
+  //        command - The corresponding command.
+  // Adds a command to this Parser that matches the provided lexeme.
+  void AddCommand(const string &name, Command *command);
+
+  // Analyzes the lexical tokens of this Parser and executes the commands.
+  void Parse();
+
+ private:
+  vector<string> tokens;
+  map<string, Command *> commands;
+};
+
+#endif //EX3_PARSER_PARSER_H_

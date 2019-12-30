33c22524e46fd86e24f55bddd14b268af888a549
Inbar Avital <53995120+InbarAvital@users.noreply.github.com>
2019-12-30 21:17:00 +0200

    Add files via upload

---
+++ project/lexer/lexer.h
@@ -0,0 +1,19 @@
+//
+// Created by inbar on 12/26/2019.
+//
+
+#ifndef PROJECT1_EXTRAFUNCTIONS_H
+#define PROJECT1_EXTRAFUNCTIONS_H
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+vector<string> addVectors(vector<string> a, vector<string> b);
+vector<string> split(string str, string splitBy);
+vector<string> splitAll(string str, vector<string> splitBy);
+vector<string> lexer(string file_name);
+class extraFunctions {
+};
+
+
+#endif //PROJECT1_EXTRAFUNCTIONS_H

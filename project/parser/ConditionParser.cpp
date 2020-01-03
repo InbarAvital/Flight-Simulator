af51aca5c832d3a1974632b91d8d63d4cdd0acb0
noamyakov <59260056+noamyakov@users.noreply.github.com>
2020-01-03 21:36:30 +0200

    Added Condition Parser
    For the If and Loop Commands.

---
+++ project/parser/ConditionParser.cpp
@@ -0,0 +1,49 @@
+//
+// Created by noam on 03/01/2020.
+//
+
+#include <iostream>
+#include "ConditionParser.h"
+#include "../expressions/Interpreter.h"
+
+bool ConditionParser::CheckCondition() {
+  // TODO: Support variables.
+  double left, right;
+  Interpreter *interpreter = new Interpreter();
+
+  // Calculates values for the condition's left and right arguments.
+  try {
+    left = interpreter->interpret(this->left_argument)->calculate();
+    right = interpreter->interpret(this->right_argument)->calculate();
+  } catch (const char* e) {
+    cout << e << endl;
+    return false;
+  }
+
+  // Checks if the condition is satisfies according to the condition's operator.
+  if (this->condition_operator == "<") {
+    return left < right;
+  } else if (this->condition_operator == ">") {
+    return left > right;
+  } else if (this->condition_operator == "<=") {
+    return left <= right;
+  } else if (this->condition_operator == ">=") {
+    return left >= right;
+  } else if (this->condition_operator == "==") {
+    return left == right;
+  } else if (this->condition_operator == "!=") {
+    return left != right;
+  } else {
+    return false;
+  }
+}
+
+void ConditionParser::InitCondition(vector<string> tokens, int index) {
+  this->left_argument = tokens[index + 1];
+  this->condition_operator = tokens[index + 2];
+  this->right_argument = tokens[index + 3];
+}
+
+Command *ConditionParser::GetCommand(string name) {
+  return this->commands[name];
+}
---
+++ project/parser/ConditionParser.h
@@ -0,0 +1,38 @@
+//
+// Created by noam on 03/01/2020.
+//
+
+#ifndef EX3_PARSER_CONDITIONPARSER_H_
+#define EX3_PARSER_CONDITIONPARSER_H_
+
+#include "../commands/Command.h"
+#include "Parser.h"
+
+// The Condition Parser is in charge of analyzing the lexical tokens
+// and executing the commands in the given order when a condition
+// is satisfies.
+class ConditionParser : public Command {
+ public:
+  // Input: p - The original parser.
+  // Constructs a ConditionParser with the commands map from the original Parser.
+  explicit ConditionParser(Parser &p): commands(p.GetCommands()) {}
+
+ protected:
+  // Output: whether the condition is satisfied or not.
+  bool CheckCondition();
+
+  // Initializes the condition's arguments and operator.
+  void InitCondition(vector<string> tokens, int index);
+
+  // Input: name - the key of some command from the commands map.
+  // Output: The matching command from the map.
+  Command *GetCommand(string name);
+
+ private:
+  map<string, Command *> commands;
+  string left_argument;
+  string right_argument;
+  string condition_operator;
+};
+
+#endif //EX3_PARSER_CONDITIONPARSER_H_

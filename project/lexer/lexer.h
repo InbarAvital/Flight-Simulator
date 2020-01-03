156d80f04e391e9681ec247c8380e8a64039b0ce
Inbar Avital <53995120+InbarAvital@users.noreply.github.com>
2020-01-04 01:16:33 +0200

    Update lexer.h

--- project/lexer/lexer.h
+++ project/lexer/lexer.h
@@ -9,9 +9,10 @@
 #include <fstream>
 using namespace std;
 vector<string> addVectors(vector<string> a, vector<string> b);
-vector<string> split(string str, string splitBy);
-vector<string> splitAll(string str, vector<string> splitBy);
+vector<string> split(string str, string splitBy, bool del);
+vector<string> splitAll(string str, vector<pair<string, int>> splitBy);
 vector<string> lexer(string file_name);
+string sub(string str, int start, int finish);
 class extraFunctions {
 };
 

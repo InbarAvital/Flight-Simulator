c0f9a5e894ced509e30d2880555b1a01add2bf04
Inbar Avital <53995120+InbarAvital@users.noreply.github.com>
2020-01-04 14:40:51 +0200

    Update lexer.h

--- project/lexer/lexer.h
+++ project/lexer/lexer.h
@@ -12,6 +12,7 @@ vector<string> addVectors(vector<string> a, vector<string> b);
 vector<string> split(string str, string splitBy, bool del);
 vector<string> splitAll(string str, vector<pair<string, int>> splitBy);
 vector<string> lexer(string file_name);
+string deleteStr(string str, string del);
 string sub(string str, int start, int finish);
 class extraFunctions {
 };
